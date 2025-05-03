16aeed397e0626313fc1a011db74cc52f48b5c77
vthani25 <vthanishkka@gmail.com>
2025-05-02 23:45:11 -0400

    Update urls.py

--- technovation/urls.py
+++ technovation/urls.py
@@ -16,14 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('translator.urls')),
+    path('', views.home, name='home'),
 ]
-
-from . import views
-path('', views.home, name='home'),
 
 from django.conf import settings
 from django.conf.urls.static import static
